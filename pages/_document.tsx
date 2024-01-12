e6ba1f4384236983157b3aa218881625efd933df
Honda Honda <honda@bezoge.com>
2024-01-12 14:21:19 +0200

    'tests'

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,12 +5,6 @@ const Home = () => {
   return (
     <Html lang="en">
       <Head>
-        <meta
-          name="description"
-          content="Full-Service Digital Marketing Agency in Dubai & Zurich. HDM specializes in Web Development, Design & UX and Digital Marketing Services. Contact us for a Digital Marketing Audit."
-        />
-        <meta property="og:image" content="/images/metalogo.jpeg" />
-
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
